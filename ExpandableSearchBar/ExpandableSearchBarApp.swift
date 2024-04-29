a59216439702071f7d7a1af6c418bbd8270b7e58
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-04-29 14:42:37 +0200

    Initial Commit

---
+++ ExpandableSearchBar/ExpandableSearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandableSearchBarApp.swift
+//  ExpandableSearchBar
+//
+//  Created by Mohsin Ali Ayub on 29.04.24.
+//
+
+import SwiftUI
+
+@main
+struct ExpandableSearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
